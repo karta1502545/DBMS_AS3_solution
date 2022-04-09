3178240bd21b9f6359ab1b43806cb13836006d0d
lawrence910426 <lawrence910426@gmail.com>
2022-04-09 23:35:53 +0800

    fix: wrong new line position

--- core-patch/src/main/java/org/vanilladb/core/query/algebra/ExplainScan.java
+++ core-patch/src/main/java/org/vanilladb/core/query/algebra/ExplainScan.java
@@ -79,7 +79,7 @@ public class ExplainScan implements Scan {
 	public boolean hasField(String fldName) { return s.hasField(fldName); }
 
 	private String getExplainString() {
-		String ans = getRecursiveExplainString(0, this.explainTree);
+		String ans = "\n" + getRecursiveExplainString(0, this.explainTree);
 		ans += String.format("Actual #recs: %d\n", this.explainTree.getOutputRecords());
 		return ans;
 	}
