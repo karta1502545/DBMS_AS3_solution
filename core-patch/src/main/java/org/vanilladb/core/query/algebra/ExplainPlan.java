386bb3c5210a7a454bfac08e6854c7d0e2c6f66b
姚潔恩 <matt1129yao@gmail.com>
2022-04-11 16:36:35 +0800

    [UPDATE] actual records

--- core-patch/src/main/java/org/vanilladb/core/query/algebra/ExplainPlan.java
+++ core-patch/src/main/java/org/vanilladb/core/query/algebra/ExplainPlan.java
@@ -10,7 +10,6 @@ import org.vanilladb.core.storage.tx.Transaction;
 public class ExplainPlan implements Plan {
     private Plan p;
     private Schema schema;
-    private Transaction tx;
 
     public ExplainPlan(Plan p) {
         this.p = p;
@@ -18,9 +17,19 @@ public class ExplainPlan implements Plan {
         schema.addField("query-plan", Type.VARCHAR(500));
     }
 
+    private int getActualRecs() {
+        Scan s = p.open();
+        s.beforeFirst();
+        int recs = 0;
+        while (s.next()) {
+            recs += 1;
+        }
+        return recs;
+    }
+
     @Override
     public String getExplainString(int level) {
-        return "\n" + p.getExplainString(0);
+        return "\n" + p.getExplainString(0) + String.format("\nActual #recs: %d", getActualRecs());
     }
 
     @Override
