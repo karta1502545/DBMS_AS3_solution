a20bfd1bbeae0adea055b9ba257fe1a73beb1515
ChangChi <sam54537@gmail.com>
2022-04-12 20:53:29 +0800

    feat: ExplainPlan

---
+++ core-patch/src/main/java/org/vanilladb/core/query/algebra/ExplainPlan.java
@@ -0,0 +1,45 @@
+package org.vanilladb.core.query.algebra;
+
+import org.vanilladb.core.sql.Schema;
+import org.vanilladb.core.sql.Type;
+import org.vanilladb.core.storage.metadata.statistics.Histogram;
+
+public class ExplainPlan implements Plan {
+
+	private Plan p;
+	private Schema schema = new Schema();
+	private Histogram hist;
+
+	public ExplainPlan(Plan p) {
+		this.p = p;
+		this.schema.addField("query-plan", Type.VARCHAR(500));
+	}
+
+	@Override
+	public Scan open() {
+		Scan s = p.open();
+		return new ExplainScan(s);
+	}
+
+	@Override
+	public long blocksAccessed() {
+		return 0;
+	}
+
+	@Override
+	public Schema schema() {
+		return schema;
+	}
+
+	@Override
+	public Histogram histogram() {
+		hist = new Histogram();
+		return hist;
+	}
+
+	@Override
+	public long recordsOutput() {
+		return 0;
+	}
+
+}
